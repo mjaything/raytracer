a133cc8c4d03ab27095dabf47971313ea40d45f9
Minjun Kim <mjaything@gmail.com>
2020-04-29 05:01:44 +0200

    Add cross_product

--- src/vector.c
+++ src/vector.c
@@ -44,3 +44,14 @@ t_vector    negate_vector(t_vector v1)
 
     return (v);
 }
+
+t_vector    cross_product(t_vector v1, t_vector v2)
+{
+    t_vector v;
+
+    v.x = v1.y * v2.z - v2.y * v1.z;
+    v.y = v1.z * v2.x - v2.z * v1.x;
+    v.z = v1.x * v2.y - v2.x * v1.y;
+
+    return (v);
+}
