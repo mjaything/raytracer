7745482e5878bbc81d95987ac1d7997a350a66f3
Minjun Kim <mjaything@gmail.com>
2020-04-29 04:38:31 +0200

    Add add_vector, subtract_vector

---
+++ src/vector.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/29 04:32:36 by min-kim           #+#    #+#             */
+/*   Updated: 2020/04/29 04:32:38 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+t_vector    add_vector(t_vector v1, t_vector v2)
+{
+    t_vector v;
+
+    v.x = v1.x + v2.x;
+    v.y = v1.y + v2.y;
+    v.z = v1.z + v2.z;
+
+    return (v);
+}
+
+t_vector    subtract_vector(t_vector v1, t_vector v2)
+{
+    t_vector v;
+
+    v.x = v1.x - v2.x;
+    v.y = v1.y - v2.y;
+    v.z = v1.z - v2.z;
+
+    return (v);
+}
