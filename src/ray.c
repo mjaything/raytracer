02ef18a59c01c44c4b40cd14337695908cfb8fc5
Minjun Kim <mjaything@gmail.com>
2020-05-03 16:28:22 +0200

    Add create_ray

---
+++ src/ray.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/03 15:12:52 by min-kim           #+#    #+#             */
+/*   Updated: 2020/05/03 15:12:53 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+t_ray     create_ray(t_tuple origin, t_tuple direction)
+{
+    t_ray ray;
+    if (!is_point(origin) || !is_vector(direction))
+        return NULL;
+    ray.origin = origin;
+    ray.direction = direction;
+    return ray;
+}
