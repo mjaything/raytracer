c02347856d26fee3c46e0ed1b301561c8a23869b
Minjun Kim <mjaything@gmail.com>
2020-05-13 11:28:37 +0200

    Modify tuples to vectors

--- src/vector.c
+++ src/vector.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.c                                           :+:      :+:    :+:   */
+/*   tuple.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,66 +12,85 @@
 
 #include "rtv1.h"
 
-t_tuple     create_tuple(double x, double y, double z, double w)
+t_vector    create_vector(double x, double y, double z)
 {
-    t_tuple tuple;
-    tuple.x = x;
-    tuple.y = y;
-    tuple.z = z;
-    tuple.w = w;
-    return tuple;
+    t_vector vector;
+
+    vector.x = x;
+    vector.y = y;
+    vector.z = z;
+    return vector;
 }
 
-t_tuple     create_point(double x, double y, double z)
+t_vector     add_vector(t_vector vector1, t_vector vector2)
 {
-    return create_tuple(x, y, z, 1.0);
+    t_vector added_vector;
+
+    added_vector.x = vector1.x + vector2.x;
+    added_vector.y = vector1.y + vector2.y;
+    added_vector.z = vector1.z + vector2.z;
+    return added_vector;
 }
 
-t_tuple     create_vector(double x, double y, double z)
+t_vector     subtract_vector(t_vector vector1, t_vector vector2)
 {
-    return create_tuple(x, y, z, 0.0);
+    t_vector subtracted_vector;
+
+    subtracted_vector.x = vector1.x - vector2.x;
+    subtracted_vector.y = vector1.y - vector2.y;
+    subtracted_vector.z = vector1.z - vector2.z;
+    return subtracted_vector;
 }
 
-t_tuple     add_vector(t_tuple vector1, t_tuple vector2)
+t_vector     negate_vector(t_vector vector)
 {
-    return create_vector(vector1.x + vector2.x, vector1.y + vector2.y, \
-                            vector1.z + vector2.z);
+    t_vector negated_vector;
+
+    negated_vector.x = -vector.x;
+    negated_vector.y = -vector.y;
+    negated_vector.z = -vector.z;
+    return negated_vector;
 }
 
-t_tuple     subtract_vector(t_tuple vector1, t_tuple vector2)
+t_vector     multiply_vector_by_scalar(t_vector vector, double c)
 {
-    return create_vector(vector1.x - vector2.x, vector1.y - vector2.y, \
-                            vector1.z + vector2.z);
+    t_vector    multiplied_vector;
+
+    multiplied_vector.x = c * vector.x;
+    multiplied_vector.y = c * vector.y;
+    multiplied_vector.z = c * vector.z;
+    return multiplied_vector;
 }
 
-t_tuple     negate_vector(t_tuple vector)
+t_vector     divide_vector_by_scalar(t_vector vector, double c)
 {
-    return create_vector(-vector.x, -vector.y, -vector.z);
+    t_vector divided_vector;
+
+    divided_vector.x = vector.x / c;
+    divided_vector.y = vector.y / c;
+    divided_vector.z = vector.z / c;
+    return divided_vector;
 }
 
-t_tuple     multiply_vector_by_scalar(t_tuple vector, double c)
-{
-    return create_vector(c * vector.x, c * vector.y, c * vector.z);
-}
-
-t_tuple     divide_vector_by_scalar(t_tuple vector, double c)
-{
-    return create_vector(vector.x / c, vector.y / c, vector.z / c);
-}
-
-double      calculate_magnitude(t_tuple vector)
-{
-    return (sqrt(vector.x * vector.x + vector.y * vector.y + \
-                vector.z * vector.z));
-}
-
-t_vector    normalize(t_tuple vector)
+double      calculate_magnitude(t_vector vector)
 {
     double magnitude;
 
-    magnitude = calculate_magnitude(v);
-    return create_vector(vector.x / magnitude, vector.y / magnitude, \
-                            vector.z / magnitude);
+    magnitude = sqrt(vector.x * vector.x + vector.y * vector.y + \
+                vector.z * vector.z);
+    return magnitude;
+}
+
+t_vector    normalize_vector(t_vector vector)
+{
+    t_vector    normalized_vector;
+    double      magnitude;
+
+    magnitude = calculate_magnitude(vector);
+    normalized_vector.x = vector.x / magnitude;
+    normalized_vector.y = vector.y / magnitude;
+    normalized_vector.z = vector.z / magnitude; 
+    return normalized_vector;
 }
 
 double      dot_product(t_tuple vector1, t_tuple vector2)
@@ -80,14 +99,14 @@ double      dot_product(t_tuple vector1, t_tuple vector2)
             vector1.z * vector2.z);
 }
 
-t_vector    cross_product(t_tuple vector1, t_tuple vector2)
+t_tuple    cross_product(t_tuple vector1, t_tuple vector2)
 {
     return create_vector(vector1.y * vector2.z - vector2.y * vector1.z,\
                             vector1.z * vector2.x - vector2.z * vector1.x,
                             vector1.x * vector2.y - vector2.x * vector1.y);
 }
 
-t_vector    hadamard_product(t_tuple vector1, t_tuple vector2)
+t_tuple    hadamard_product(t_tuple vector1, t_tuple vector2)
 {
     return create_vector(vector1.x * vector2.x,\
                             vector1.y * vector2.y,
