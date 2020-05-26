96572e6c8bbdadecb32fe69c6eea14fbceaf0fad
Minjun Kim <mjaything@gmail.com>
2020-05-26 12:34:35 +0200

    Add rotation functions

--- src/rotate.c
+++ src/rotate.c
@@ -12,39 +12,45 @@
 
 #include "rtv1.h"
 
-
-void        rotation_x_axis(t_vector *vector, double degrees)
+void        rotation_x_axis(t_vector *vector, double degree)
 {
     t_vector    rotated_vector;
     double      radian;
 
-    radian = degrees_to_radian(degrees);
+    radian = degree_to_radian(degree);
     rotated_vector.x = vector->x;
     rotated_vector.y = vector->y * cos(radian) + vector->z * sin(radian);
     rotated_vector.z = vector->y * -sin(radian) + vector->z * cos(radian);
     *vector = rotated_vector;
 }
 
-void        rotation_y_axis(t_vector *vector, double degrees)
+void        rotation_y_axis(t_vector *vector, double degree)
 {
     t_vector    rotated_vector;
     double      radian;
 
-    radian = degrees_to_radian(degrees);
+    radian = degree_to_radian(degree);
     rotated_vector.x = vector->x * cos(radian) + vector->z * -sin(radian);
     rotated_vector.y = vector->y;
     rotated_vector.z = vector->x * sin(radian) + vector->z * cos(radian);
     *vector = rotated_vector;
 }
 
-void        rotation_z_axis(t_vector *vector, double degrees)
+void        rotation_z_axis(t_vector *vector, double degree)
 {
     t_vector    rotated_vector;
     double      radian;
 
-    radian = degrees_to_radian(degrees);
+    radian = degree_to_radian(degree);
     rotated_vector.x = vector->x * cos(radian) + vector->y * sin(radian);
     rotated_vector.y = vector->x * -sin(radian) + vector->y * cos(radian);
     rotated_vector.z = vector->z;
     *vector = rotated_vector;
+}
+
+void        rotate(t_vector *vector, t_vector rotation_angle)
+{
+    rotation_x_axis(vector, rotation_angle.x);
+    rotation_y_axis(vector, rotation_angle.y);
+    rotation_z_axis(vector, rotation_angle.z);
 }
