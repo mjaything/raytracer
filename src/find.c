35368fd5c1b5e1e2e3b841b8ea6901934ec20aec
Minjun Kim <mjaything@gmail.com>
2020-05-27 23:07:10 +0200

    Modify vector name in find_surface_normal function

--- src/find.c
+++ src/find.c
@@ -14,22 +14,22 @@
 
 void    find_surface_normal(t_env *env, t_object *object)
 {
-    t_vector    temp_vector;
+    t_vector    surface_normal;
 
     if (object->shape == SPHERE)
     {
-        temp_vector = subtract_vector(object->origin, env->ray.hit);
-        object->surface_normal = normalize_vector(temp_vector);
+        surface_normal = subtract_vector(object->origin, env->ray.hit);
+        object->surface_normal = normalize_vector(surface_normal);
     }
     else if (object->shape == CONE || object->shape == CYLINDER)
     {
-        temp_vector = subtract_vector(object->origin, env->ray.hit);
-        temp_vector = create_vector(temp_vector.x, 0.0, temp_vector.z)
-        object->surface_normal = normalize_vector(temp_vector);
+        surface_normal = subtract_vector(object->origin, env->ray.hit);
+        surface_normal.y = 0.0;
+        object->surface_normal = normalize_vector(surface_normal);
     }
     else if (object->shape == PLANE)
     {
-        temp_vector = subtract_vector(object->origin, object->direction);
-        object->surface_normal = normalize_vector(temp_vector);
+        surface_normal = subtract_vector(object->origin, object->direction);
+        object->surface_normal = normalize_vector(surface_normal);
     }
 }
