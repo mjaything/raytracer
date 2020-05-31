9540ccf980d55409b7bd89a60b3431eebf009435
Minjun Kim <mjaything@gmail.com>
2020-05-31 20:14:03 +0200

    Add trace_color function

--- src/render.c
+++ src/render.c
@@ -12,6 +12,34 @@
 
 #include "rtv1.h"
 
+void    trace_color(t_env *env, t_object *object, \
+                    double *nonnegative_min_intersection, double *intersection)
+{
+    t_light     *light;
+    t_vector reflection_ambient;
+    t_vector reflection_diffuse;
+    t_vector reflection_specular;
+
+    light = env->light;
+    env->color = create_vector(0.0, 0.0, 0.0);
+    while ((light = light->next) != NULL)
+    {
+        configure_light_source(env, light);
+        find_surface_normal(env, object);
+        find_shadows(env, object, nonnegative_min_intersection, intersection);
+        reflection_ambient = \
+            multiply_vector_by_scalar(light->color, \
+                                        object->material.reflection_ambient);
+        reflection_diffuse = calculate_diffuse_contribution(env, object, light);
+        reflection_specular = calculate_specular_contribution(env, object, light);
+        env->color_intersection = add_vector(reflection_ambient, reflection_diffuse);
+        env->color_intersection = add_vector(env->color.intersection, reflection_specular);
+        env->color_intersection = multiply_vector_by_scalar(env->color_intersection, env->shadow);
+        env->color_intersection = multiply_vector_by_scalar(env->color_intersection, object->material.surface_color);
+        env->color = add_vector(env->color, env->color_intersection);
+    }
+}
+
 void    trace_reflection(t_env *env, t_object *object)
 {
     if (env->trace_recursion_depth < env->camera.recursion_threshold)
