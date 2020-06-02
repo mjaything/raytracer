fd6104312d620e73c32851f19ad7532a89f24363
Minjun Kim <mjaything@gmail.com>
2020-06-02 04:04:26 +0200

    Add ray_sphere_intersection function

--- src/sphere.c
+++ src/sphere.c
@@ -12,32 +12,21 @@
 
 #include "rtv1.h"
 
-double  *ray_sphere_intersection(t_object sphere, t_ray ray)
+double  ray_sphere_intersection(t_env *env, t_object *object)
 {
-    t_tuple sphere_to_ray_vector;
-    double  a;
-    double  b;
-    double  c;
-    double  discriminant;
-    double  intersection1;
-    double  intersection2;
-    double  intersections[2];
-    double  temp;
+    double      a;
+    double      b;
+    double      c;
+    double      discriminant;
+    t_vector    sphere_to_ray_vector;
 
-    sphere_to_ray_vector = subtract_vector(ray.origin - create_point(0, 0, 0));
-    a = dot_product(ray.direction, ray.direction);
-    b = 2 * dot_product(ray.direction, sphere_to_ray_vector);
-    c = dot_product(sphere_to_ray_vector, sphere_to_ray_vector) - 1;
-    discriminant = b * b - 4 * a * c;
-    if (discriminant < 0)
-        return intersections;
-    intersections[0] = (-b - sqrt(discriminant)) / (2 * a);
-    intersections[1] = (-b + sqrt(discriminant)) / (2 * a);
-    if (intersections[0] > intersections[1])
-    {
-        temp = intersections[0];
-        intersections[0] = intersections[1];
-        intersections[1] = temp;
-    }
-    return intersections;
+    sphere_to_ray_vector = subtract_vector(env->ray.origin, object->origin);
+    a = dot_product(env->ray.direction, env->ray.direction);
+    b = 2 * dot_product(sphere_to_ray_vector, env->ray.direction);
+    c = dot_product(sphere_to_ray_vector, sphere_to_ray_vector) - object->scale;
+    discriminant = calculate_discriminant(a, b, c);
+    if (discriminant < EPSILON)
+        return -1.0;
+    else
+        return (-b - sqrt(discriminant)) / (2 * a);
 }
