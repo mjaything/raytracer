e3b2c82c36f24afe690fa205bece6cf4f4967617
Minjun Kim <mjaything@gmail.com>
2020-06-02 17:37:27 +0200

    organize configure_light_source function

--- src/ray.c
+++ src/ray.c
@@ -12,7 +12,7 @@
 
 #include "rtv1.h"
 
-void        configure_light_source(t_env *env, t_light light)
+void        configure_light_source(t_env *env, t_light *light)
 {
     env->ray.origin = light->origin;
     env->ray.direction = normalize_vector(subtract_vector(env->ray.hit, \
