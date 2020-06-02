e2a0eff57998ad7bc78f5205c1114123bd20321b
Minjun Kim <mjaything@gmail.com>
2020-06-02 10:34:49 +0200

    Add ray_cylinder_intersection function

---
+++ src/cylinder.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cylinder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: min-kim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/07 20:09:59 by min-kim           #+#    #+#             */
+/*   Updated: 2020/05/07 20:10:01 by min-kim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+double  ray_cylinder_intersection(t_env *env, t_object *object)
+{
+    double      a;
+    double      b;
+    double      c;
+    double      discriminant;
+    t_vector    cylinder_to_ray_vector;
+
+    cylinder_to_ray_vector = subtract_vector(env->ray.origin, object->origin);
+    a = ft_pow(env->ray.direction.x, 2) + ft_pow(env->ray.direction.z, 2);
+    b = 2 * env->ray.origin.x * env->ray.direction.x + \
+        2 * env->ray.origin.z * env->ray.direction.z;
+    c = ft_pow(env->ray.origin.x, 2) + ft_pow(env->ray.origin.z, 2) - \
+        object->scale;
+    discriminant = calculate_discriminant(a, b, c);
+    if (discriminant < EPSILON)
+        return -1.0;
+    return (-b - sqrt(discriminant)) / (2 * a);
+}
